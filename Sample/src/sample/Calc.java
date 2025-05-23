704cc6e4e5edeacd0ac691dfcca552bea40cf26f
moki <tomoki.saito.a084@dirbato.co.jp>
2025-05-23 10:49:04 +0900

    samplecommit1

---
+++ Sample/src/sample/Calc.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Calc {
+
+    public static void main(String[] args) {
+        var num1 = 100;
+        var num2 = 10;
+        
+        // 足し算
+        var result1 = num1 + num2;
+        System.out.println(result1);
+        
+        // 引き算
+        var result2 = num1 - num2;
+        System.out.println(result2);
+        
+        //　掛け算
+        var result3 = num1 * num2;
+        System.out.println(result3);
+        
+        // 割り算
+        var result4 = num1 / num2;
+        System.out.println(result4);
+
+    }
+
+}
---
+++ Sample/src/sample/Variable.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        var number1 = 1;
+        System.out.println(number1);
+        
+        String str1;
+        str1 = "abc";
+        System.out.println(str1);
+        
+        var secondsPerDay = 60*60*24;
+        System.out.println(secondsPerDay);
+
+    }
+
+}
